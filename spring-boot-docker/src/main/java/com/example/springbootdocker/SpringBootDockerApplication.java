2ccc9df42c48dc7e4d528f2da204fdc7f6626733
Andres Galeano <afgaleano91@misena.edu.co>
2021-10-09 00:51:55 -0500

    Se crea proyect base y docker compose para base de datos

--- spring-boot-docker/src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
+++ spring-boot-docker/src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
@@ -2,14 +2,16 @@ package com.example.springbootdocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @RestController
+@SpringBootApplication
 public class SpringBootDockerApplication {
 
-	@RequestMaping("/")
+	@RequestMapping("/")
 	public String home() {
-		return  "Hello Docker World";
+		return "Hello Docker World";
 	}
 
 	public static void main(String[] args) {
