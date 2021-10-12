8c023614e3ad3344a83d1944daf7394755c1ebe3
Andres Galeano <afgaleano91@misena.edu.co>
2021-10-11 22:55:47 -0500

    Se complementa el modelo hijo

--- spring-boot-docker/src/main/java/com/example/springbootdocker/model/Child.java
+++ spring-boot-docker/src/main/java/com/example/springbootdocker/model/Child.java
@@ -1,2 +1,38 @@
-package com.example.springbootdocker.model;public class Child {
+package com.example.springbootdocker.model;
+
+public class Child {
+
+    private int id_child;
+    private int id_father;
+    private int id_mother;
+
+    public Child(int id_child, int id_father, int id_mother) {
+        this.id_child = id_child;
+        this.id_father = id_father;
+        this.id_mother = id_mother;
+    }
+
+    public int getId_child() {
+        return id_child;
+    }
+
+    public void setId_child(int id_child) {
+        this.id_child = id_child;
+    }
+
+    public int getId_father() {
+        return id_father;
+    }
+
+    public void setId_father(int id_father) {
+        this.id_father = id_father;
+    }
+
+    public int getId_mother() {
+        return id_mother;
+    }
+
+    public void setId_mother(int id_mother) {
+        this.id_mother = id_mother;
+    }
 }
