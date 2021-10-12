36fc91c93764e538d1735d4df01761d3257d2c5d
Andres Galeano <afgaleano91@misena.edu.co>
2021-10-11 22:56:19 -0500

    Se complementa el modelo padre

--- spring-boot-docker/src/main/java/com/example/springbootdocker/model/Father.java
+++ spring-boot-docker/src/main/java/com/example/springbootdocker/model/Father.java
@@ -1,2 +1,18 @@
-package com.example.springbootdocker.model;public class Father {
+package com.example.springbootdocker.model;
+
+public class Father {
+
+    private int id_father;
+
+    public Father(int id_father) {
+        this.id_father = id_father;
+    }
+
+    public int getId_father() {
+        return id_father;
+    }
+
+    public void setId_father(int id_father) {
+        this.id_father = id_father;
+    }
 }
