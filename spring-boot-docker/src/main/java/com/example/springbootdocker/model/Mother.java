0bba442320922ba6e114916f42070f2823702e2f
Andres Galeano <afgaleano91@misena.edu.co>
2021-10-11 22:56:50 -0500

    Se complementa el modelo madre

--- spring-boot-docker/src/main/java/com/example/springbootdocker/model/Mother.java
+++ spring-boot-docker/src/main/java/com/example/springbootdocker/model/Mother.java
@@ -1,2 +1,18 @@
-package com.example.springbootdocker.model;public class Mother {
+package com.example.springbootdocker.model;
+
+public class Mother {
+
+    private int id_mother;
+
+    public Mother(int id_mother) {
+        this.id_mother = id_mother;
+    }
+
+    public int getId_mother() {
+        return id_mother;
+    }
+
+    public void setId_mother(int id_mother) {
+        this.id_mother = id_mother;
+    }
 }
